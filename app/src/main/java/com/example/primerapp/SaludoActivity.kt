8b3ebf287afb6e1e5d1956cca7b37288ec1beaeb
RodrigoZamoraDAM2 <rodzam799@alu.centroafuera.es>
2022-10-27 14:25:09 +0200

    FINALIZADO

--- app/src/main/java/com/example/primerapp/SaludoActivity.kt
+++ app/src/main/java/com/example/primerapp/SaludoActivity.kt
@@ -2,10 +2,20 @@ package com.example.primerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SaludoActivity : AppCompatActivity() {
+
+    private lateinit var txtSaludo : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_saludo)
+
+        txtSaludo = findViewById(R.id.txtSaludo)
+
+        txtSaludo.text = "Hola ${intent.getStringExtra("NOMBRE")}"
+
     }
 }
