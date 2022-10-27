b228a4fe2e6a4b5a200a81be0924c1f545bef238
RodrigoZamoraDAM2 <rodzam799@alu.centroafuera.es>
2022-10-27 12:10:27 +0200

    aun inacabado

---
+++ app/src/main/java/com/example/primerapp/SaludoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.primerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SaludoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saludo)
+    }
+}
